039b651868d7fa9f32b1507be9da3f82b84626c6
foreignbill <494477585@qq.com>
2022-06-06 03:02:50 +0000

    init commit

---
+++ mmfewshot/utils/runner.py
@@ -0,0 +1,144 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import time
+import warnings
+
+import mmcv
+from mmcv.runner import EpochBasedRunner
+from mmcv.runner.builder import RUNNERS
+from torch.utils.data import DataLoader
+from tensorboardX import SummaryWriter
+import numpy as np
+
+@RUNNERS.register_module()
+class InfiniteEpochBasedRunner(EpochBasedRunner):
+    """Epoch-based Runner supports dataloader with InfiniteSampler.
+
+    The workers of dataloader will re-initialize, when the iterator of
+    dataloader is created. InfiniteSampler is designed to avoid these time
+    consuming operations, since the iterator with InfiniteSampler will never
+    reach the end.
+    """
+    # TODO: add tensor_writer
+    def __init__(self,
+                 model,
+                 batch_processor=None,
+                 optimizer=None,
+                 work_dir=None,
+                 logger=None,
+                 meta=None,
+                 max_iters=None,
+                 max_epochs=None):
+        super().__init__(model, batch_processor, optimizer, work_dir, logger, meta, max_iters, max_epochs)
+        if work_dir is not None and work_dir == '/export':
+            self.tensor_writer = SummaryWriter('./tensorboard_log/')
+        else:
+            self.tensor_writer = SummaryWriter(f'{work_dir}/tensorboard_log')
+
+    # TODO: derived from EpochBasedRunner
+    def run_iter(self, data_batch, train_mode, **kwargs):
+        if self.batch_processor is not None:
+            outputs = self.batch_processor(
+                self.model, data_batch, train_mode=train_mode, **kwargs)
+        elif train_mode:
+            outputs = self.model.train_step(data_batch, self.optimizer,
+                                            **kwargs)
+        else:
+            outputs = self.model.val_step(data_batch, self.optimizer, **kwargs)
+        if not isinstance(outputs, dict):
+            raise TypeError('"batch_processor()" or "model.train_step()"'
+                            'and "model.val_step()" must return a dict')
+        
+        if 'log_vars' in outputs:
+            self.log_buffer.update(outputs['log_vars'], outputs['num_samples'])
+        self.outputs = outputs
+    
+    # TODO: derived from EpochBasedRunner
+    def run(self, data_loaders, workflow, max_epochs=None, **kwargs):
+        """Start running.
+
+        Args:
+            data_loaders (list[:obj:`DataLoader`]): Dataloaders for training
+                and validation.
+            workflow (list[tuple]): A list of (phase, epochs) to specify the
+                running order and epochs. E.g, [('train', 2), ('val', 1)] means
+                running 2 epochs for training and 1 epoch for validation,
+                iteratively.
+        """
+        assert isinstance(data_loaders, list)
+        assert mmcv.is_list_of(workflow, tuple)
+        assert len(data_loaders) == len(workflow)
+        if max_epochs is not None:
+            warnings.warn(
+                'setting max_epochs in run is deprecated, '
+                'please set max_epochs in runner_config', DeprecationWarning)
+            self._max_epochs = max_epochs
+
+        assert self._max_epochs is not None, (
+            'max_epochs must be specified during instantiation')
+
+        for i, flow in enumerate(workflow):
+            mode, epochs = flow
+            if mode == 'train':
+                self._max_iters = self._max_epochs * len(data_loaders[i])
+                break
+
+        work_dir = self.work_dir if self.work_dir is not None else 'NONE'
+        self.logger.info('Start running, host: %s, work_dir: %s',
+                         mmcv.runner.get_host_info(), work_dir)
+        self.logger.info('Hooks will be executed in the following order:\n%s',
+                         self.get_hook_info())
+        self.logger.info('workflow: %s, max: %d epochs', workflow,
+                         self._max_epochs)
+        self.call_hook('before_run')
+
+        while self.epoch < self._max_epochs:
+            self.tensor_writer.add_scalar('progress', self.epoch / self._max_epochs, global_step=self.epoch)
+
+            mode, epochs = flow
+            if isinstance(mode, str):  # self.train() below
+                if not hasattr(self, mode):
+                    raise ValueError(
+                        f'runner has no method named "{mode}" to run an '
+                        'epoch')
+                epoch_runner = getattr(self, mode)
+            else:
+                raise TypeError(
+                    'mode in workflow must be a str, but got {}'.format(
+                        type(mode)))
+            for _ in range(epochs):
+                if mode == 'train' and self.epoch >= self._max_epochs:
+                    break
+                epoch_runner(data_loaders[i], **kwargs)
+
+        time.sleep(1)  # wait for some hooks like loggers to finish
+        self.call_hook('after_run')
+
+    def train(self, data_loader: DataLoader, **kwargs) -> None:
+        self.model.train()
+        self.mode = 'train'
+        self.data_loader = data_loader
+        self._max_iters = self._max_epochs * len(self.data_loader)
+        self.call_hook('before_train_epoch')
+        time.sleep(2)  # Prevent possible deadlock during epoch transition
+
+        # To reuse the iterator, we only create iterator once and bind it
+        # with runner. In the next epoch, the iterator will be used against
+        if not hasattr(self, 'data_loader_iter'):
+            self.data_loader_iter = iter(self.data_loader)
+
+        # The InfiniteSampler will never reach the end, but we set the
+        # length of InfiniteSampler to the actual length of dataset.
+        # The length of dataloader is determined by the length of sampler,
+        # when the sampler is not None. Therefore, we can simply forward the
+        # whole dataset in a epoch by length of dataloader.
+
+        for i in range(len(self.data_loader)):
+            data_batch = next(self.data_loader_iter)
+            self._inner_iter = i
+            self.call_hook('before_train_iter')
+            self.run_iter(data_batch, train_mode=True, **kwargs)
+            self.call_hook('after_train_iter')
+            self._iter += 1
+
+        self.call_hook('after_train_epoch')
+        self._epoch += 1

4b005fdcd310c56e3ba8207faf69b525389d9ca3
foreignbill <494477585@qq.com>
2022-06-10 03:25:12 +0000

    remove useless config file

--- configs/baseline.py
+++
@@ -1,6 +0,0 @@
-#### model
-model = dict(
-    type='Baseline',
-    backbone=dict(type='Conv4'),
-    head=dict(type='LinearHead', num_classes=100, in_channels=1600),
-    meta_test_head=dict(type='LinearHead', num_classes=5, in_channels=1600))
